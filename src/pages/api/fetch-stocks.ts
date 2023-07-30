82c980b60b4e838e9698280da0f7a322cdcb1941
luser0 <merooooyasser@gmail.com>
2023-07-30 19:27:28 +0300

    api: new fetch stocks endpoint

---
+++ src/pages/api/fetch-stocks.ts
@@ -0,0 +1,26 @@
+import { TtickersResJson } from "@/types/TtickersResJson";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  let fetchUrl = "";
+  if (req.body.firstFetch) {
+    fetchUrl =
+      "https://api.polygon.io/v3/reference/tickers?market=stocks&exchange=XNAS&active=true&order=asc&limit=50&sort=name&apiKey=" +
+      process.env.POLYGON_API_KEY;
+  } else {
+    fetchUrl =
+      req.body.nextTickerFetchUrl + "&apiKey=" + process.env.POLYGON_API_KEY;
+  }
+  try {
+    const stocksRes = await fetch(fetchUrl);
+    try {
+      const stocksResJson: TtickersResJson = await stocksRes.json();
+
+      res.status(stocksRes.status).json(stocksResJson);
+    } catch (error) {
+      res.status(500).send("");
+    }
+  } catch (error) {
+    res.status(500).send("");
+  }
+};
