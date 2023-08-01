e92172a9190182544b087209441c58ca398fc8a8
luser0 <merooooyasser@gmail.com>
2023-08-01 19:52:55 +0300

    API-FIX : fetch cache with new postfix  on fetch-stock-details

--- src/pages/api/fetch-stock-details.ts
+++ src/pages/api/fetch-stock-details.ts
@@ -11,7 +11,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     return;
   }
 
-  let cache = await redis.get(req.body.ticker.toUpperCase());
+  let cache = await redis.get(req.body.ticker.toUpperCase() + "-details");
   if (cache) {
     res.json(cache);
     return;
