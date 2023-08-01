ba6e31c5358868e9cb0aa9015a52fcafbc8b85a0
luser0 <merooooyasser@gmail.com>
2023-08-01 19:59:25 +0300

    API : add redis cache to fetch-prev-day

--- src/pages/api/fetch-prev-day.ts
+++ src/pages/api/fetch-prev-day.ts
@@ -1,7 +1,22 @@
 import { TtickersResJson } from "@/types/TtickersResJson";
+import Redis from "ioredis";
+
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const redis = new Redis(process.env.REDIS_URL as string);
+
+  if (!req.body.ticker) {
+    res.status(400).send("");
+    return;
+  }
+
+  let cache = await redis.get(req.body.ticker.toUpperCase() + "-prev-day");
+  if (cache) {
+    res.send(cache);
+    return;
+  }
+
   try {
     const stockPrevDayRes = await fetch(
       `https://api.polygon.io/v2/aggs/ticker/${req.body.ticker.toUpperCase()}/prev?adjusted=true&apiKey=${
@@ -11,6 +26,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     try {
       const stockPrevDayResJson: TtickersResJson = await stockPrevDayRes.json();
       res.status(stockPrevDayRes.status).json(stockPrevDayResJson);
+      await redis.set(
+        req.body.ticker + "-prev-day",
+        JSON.stringify(stockPrevDayResJson),
+        "EX",
+        60
+      );
     } catch (error) {
       res.status(500).send("");
     }
