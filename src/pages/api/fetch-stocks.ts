c47bec6c926fb8a892cc3fef3c100567f6bedc0e
luser0 <merooooyasser@gmail.com>
2023-08-01 19:33:55 +0300

    API : add redis cache to fetch-stocks

--- src/pages/api/fetch-stocks.ts
+++ src/pages/api/fetch-stocks.ts
@@ -1,22 +1,50 @@
 import { TtickersResJson } from "@/types/TtickersResJson";
+import Redis from "ioredis";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  let fetchUrl = "";
-  if (req.query.firstFetch) {
+  let fetchUrl: string;
+
+  let cacheToSet: string;
+  const redis = new Redis(process.env.REDIS_URL as string);
+  let cache;
+
+  //i can prob refactor this into somthing nicer but this works for now
+  if (req.query.firstFetch === "true") {
     fetchUrl =
       "https://api.polygon.io/v3/reference/tickers?market=stocks&exchange=XNAS&active=true&order=asc&limit=50&sort=name&apiKey=" +
       process.env.POLYGON_API_KEY;
-  } else {
+
+    cache = await redis.get("firstFetch");
+    cacheToSet = "firstFetch";
+
+    if (cache) cache = JSON.parse(cache);
+  } else if (
+    req.query.nextTickerFetchUrl &&
+    typeof req.query.nextTickerFetchUrl === "string"
+  ) {
     fetchUrl =
       req.query.nextTickerFetchUrl + "&apiKey=" + process.env.POLYGON_API_KEY;
+
+    cache = await redis.get("nextTickerFetchUrl");
+    cacheToSet = req.query.nextTickerFetchUrl;
+
+    if (cache) cache = JSON.parse(cache);
+  } else {
+    res.status(500).json({ err: "invalid query" });
+    return;
   }
+
   try {
+    if (cache) {
+      res.send(cache);
+      return;
+    }
     const stocksRes = await fetch(fetchUrl);
     try {
       const stocksResJson: TtickersResJson = await stocksRes.json();
-
       res.status(stocksRes.status).json(stocksResJson);
+      await redis.set(cacheToSet, JSON.stringify(stocksResJson), "EX", 60);
     } catch (error) {
       res.status(500).send("");
     }
