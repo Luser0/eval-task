a90a9dc1a9f80307206b4be0f3e3062ae2a0d980
luser0 <merooooyasser@gmail.com>
2023-08-02 15:01:39 +0300

    FIX : _document Head Warning

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Ramen Code</title>
-      </Head>
+      <Head />
 
       <body>
         <Main />
